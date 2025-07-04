62e1bd58be5aaebe268a30d4a867f6cafcab89f8
mj99k14 <ekfkawnl644@gmail.com>
2025-07-04 22:22:11 +0900

    lab2

--- backend/src/action.php
+++ backend/src/action.php
@@ -1,9 +1,12 @@
 <?php
 $sum = 0;
 for ($x = 1; $x <= 5; $x++) {
-    $sum += $_POST["value"];
+    $sum += (int)$_POST["value$x"];
+
+    echo "입력값:"  . (int)$_POST["value$x"];
 }
-echo $sum;
+
+echo "평균" . $sum;
 
 // $a = $_POST["value"];
 // $b =  $_POST['value'];
