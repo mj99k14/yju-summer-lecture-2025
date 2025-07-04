608da0ab62b48858a818fba659deaa8e9690e144
mj99k14 <ekfkawnl644@gmail.com>
2025-07-04 21:30:09 +0900

    Lab2

---
+++ backend/src/action.php
@@ -0,0 +1,17 @@
+<?php
+$sum = 0;
+for ($x = 1; $x <= 5; $x++) {
+    $sum += $_POST["value"];
+}
+echo $sum;
+
+// $a = $_POST["value"];
+// $b =  $_POST['value'];
+// $c =  $_POST['value'];
+// $d =  $_POST['value'];
+// $e =  $_POST['value'];
+
+// $sum = $a + $b + $c + $d + $e;
+
+
+// echo $sum;
