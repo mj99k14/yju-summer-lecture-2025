9157e764c88ae5afa74a0d787eac2c190f9b77c4
mj99k14 <ekfkawnl644@gmail.com>
2025-07-11 16:31:56 +0900

    get& post

--- backend/src/1.php
+++ backend/src/1.php
@@ -1,10 +1,35 @@
 <?php
 
 print_r($_POST);
+function http_response_error($msg)
+{
+    http_response_code(400);
+    echo $msg . "<br>";
+    exit;
+}
 
-// print("<br>");
-// print($_GET['bar']);
+// POST or GET 입력 값 사용 전, 반드시 입력 값에 대한 검증이 필요하다.
 
-// print("<br>");
+// 1. 입력 값의 존재 여부
+if (!isset($_POST["id"]) || !isset($_POST["pw"])) {
+    http_response_error("입력값을 확인하세요.");
+}
 
-// print($_GET['name']);
+// 2. 문자열 전처리
+$id = trim($_POST["id"]);
+$pw = trim($_POST["pw"]);
+
+//3. option : 각 필드별 특성 처리
+if (!is_numeric($pw)) {
+    http_response_error("패스워드는 숫자로 구성됩니다.");
+}
+
+if ($id === '') {
+    http_response_error("id를 입력하세요.");
+}
+
+var_dump($_POST);
+
+echo "입력값 검증 완료<br>";
+
+var_dump($pw);
